b7249d809e78fde9e7910e91f4bd7418bbc54ab1
TrungMoon <thunderlight0235@gmail.com>
2023-02-13 18:53:50 +0700

    ngay 2

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -16,22 +17,19 @@ public class GamePanel extends JPanel implements Runnable {
 	final int scale = 3;
 
 	public final int titleSize = originalTitleSize * scale; // 48*48
-	final int maxScreenCol = 16; // <- ->
-	final int maxScreenRow = 12; // /|\
-	final int screenWidth = titleSize * maxScreenCol; // 768 pix
-	final int screenHeight = titleSize * maxScreenRow; // 576 pix
+	public final int maxScreenCol = 16; // <- ->
+	public final int maxScreenRow = 12; // /|\
+	public final int screenWidth = titleSize * maxScreenCol; // 768 pix
+	public final int screenHeight = titleSize * maxScreenRow; // 576 pix
 
 	// FPS
 	int FPS = 60;
-
+	
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	Player player = new Player(this, keyH);
 
-	// set player's default postion
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
 
 	public GamePanel() {
 
@@ -122,7 +120,9 @@ public class GamePanel extends JPanel implements Runnable {
 		super.paintComponent(g);
 
 		Graphics2D g2 = (Graphics2D)g;
-
+		
+		tileM.draw(g2);
+		
 		player.draw(g2);
 		
 		g2.dispose();
